53c1da944f9118e0c47047beee5d5028121a1cf9
rimi <fla0315@naver.com>
2023-06-18 17:13:00 +0900

    JPA 기본 및 JPQL 기초

--- src/main/java/helloJPA/JpaMain.java
+++ src/main/java/helloJPA/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -20,6 +21,22 @@ public class JpaMain {
             //em.persist(member);
 
             Member findMember = em.find(Member.class,1L);
+
+            //이렇게 JPQL에서 조회할 때 member라는 테이블을 참조하는 것이 아닌
+            //객체 자체를 참조한다
+            //List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+
+            //객체를 가져오는것이 굉장한 이점들이 있다 ex> 페이징처리 첫번째 페이지 마지막페이지. 처리 가능
+            List<Member> result = em.createQuery("select m from Member as m",Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
 
             findMember.setName("denver93");
 
